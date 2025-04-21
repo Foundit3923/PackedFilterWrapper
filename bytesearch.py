ca009feb06b66aaacc7ec906c16a5784214913c8
Michael Olson <olsonmichael32@gmail.com>
2025-04-21 10:58:46 -0400

    Add bytesearch.py and filter.py

---
+++ bytesearch.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a780c3b90adcbd4cd2a6c8ab4264dcc14fa78b65aeb4933cfc16c87fa404d952
+size 8315
---
+++ filter.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:141427187ec237a650271ad74602e3e72b72d6b21ef0889c279527c4a0fb3d0e
+size 2803
