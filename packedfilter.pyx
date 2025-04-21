06b632b9b9d227e2251879cde02e9588848bab32
Michael Olson <olsonmichael32@gmail.com>
2025-04-21 11:45:50 -0400

    add return statement to packefilter.main()

--- packedfilter.pyx
+++ packedfilter.pyx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32dd04b11197ede6e7b32169853949f6a3a35fa6b06d7efb3c231584e305f68d
-size 11793
+oid sha256:e59a72f73ebc1a3ab09ab9b27da194d05540b2ffcc5ce0f8aa7d66d5650c1047
+size 11820
